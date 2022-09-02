7da0b4c8262df0b605020a7bd37f4531d3183526
陈湛鹏 <2283487595@qq.com>
2022-09-02 18:00:10 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,38 @@
  * @Autor: czp
  * @Date: 2022-09-02 16:06:39
  * @LastEditors: czp
- * @LastEditTime: 2022-09-02 17:20:10
+ * @LastEditTime: 2022-09-02 17:38:54
  */
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import { resolve } from 'path'
 
 
-export default defineConfig({
-    envDir: resolve(process.cwd(), './env'),
-    envPrefix: 'ENV_',
-    resolve: {
-        alias: {
-            '@': resolve(process.cwd(), './src'),
-            '~C': resolve(process.cwd(), './src/components'),
-            '~V': resolve(process.cwd(), './src/views')
-        }
-    },
-    plugins: [vue(),
-    Components({
-        resolvers: [VantResolver()],
-    })]
+export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
+
+    return {
+        envDir: resolve(process.cwd(), './env'),
+        envPrefix: 'ENV_',
+        resolve: {
+            alias: {
+                '@': resolve(process.cwd(), './src'),
+                '~C': resolve(process.cwd(), './src/components'),
+                '~V': resolve(process.cwd(), './src/views')
+            }
+        },
+        build: {
+
+        },
+        plugins: [
+            vue(),
+            Components({
+                resolvers: [VantResolver()],
+            }),
+            createHtmlPlugin({
+                minify: true
+            })]
+    }
 })
